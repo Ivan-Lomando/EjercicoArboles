0de482ec50c92b3367ec6d45b988693c55aa0386
unknown <ivan.lomando@ucu.edu.uy>
2022-11-14 10:28:30 -0300

    cambios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,24 +7,46 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Persona P1 = new Persona("ivan",21);
+            Persona P2 = new Persona("CR7",20);
+            Persona P3 = new Persona("Ironman",19);
+            Persona P4 = new Persona("Messi",18);
+            Persona P5 = new Persona("Hulk",17);
+            Persona P6 = new Persona("Blackpanter",16);
+            Persona P7 = new Persona("Naruto",15);
+            Persona P8 = new Persona("sasuke",14);
+        
+            Node<Persona> np1 = new Node<Persona>(P1);
+            Node<Persona> np2 = new Node<Persona>(P2);
+            Node<Persona> np3 = new Node<Persona>(P3);
+            Node<Persona> np4 = new Node<Persona>(P4);
+            Node<Persona> np5 = new Node<Persona>(P5);
+            Node<Persona> np6 = new Node<Persona>(P6);
+            Node<Persona> np7 = new Node<Persona>(P7);
+            Node<Persona> np8 = new Node<Persona>(P8);
 
-            n1.AddChildren(n2);
-            n1.AddChildren(n3);
-
-            n2.AddChildren(n4);
-            n2.AddChildren(n5);
-
-            n3.AddChildren(n6);
-            n3.AddChildren(n7);
-
-            // visitar el árbol aquí
+            np1.AddChildren(np2);
+            np1.AddChildren(np3);
+            np2.AddChildren(np4);
+            np2.AddChildren(np5);
+            np3.AddChildren(np6);
+            np3.AddChildren(np7);
+            np4.AddChildren(np8);
+        //------------------Parte 3------------------------------------------------------------------------------
+           SumaEdades sum = new SumaEdades();
+           sum.Visit(np1);
+           Console.WriteLine($"La suma de las edades es: {sum.sumaedad}");
+        //------------------Parte 4A------------------------------------------------------------------------------
+           EdadMayor sum1 = new EdadMayor();
+           sum1.Visit(np1);
+           Console.WriteLine($"El mas viejo es {sum1.mayor} con una edad de {sum1.Edad}");
+        //------------------Parte 4B--------------------------------------------------------------------------------
+            NombreLargo sum2 = new NombreLargo();
+            sum2.Visit(np1);
+            Console.WriteLine($"El nombre mas largo es {sum2.mayor1} Con un total de {sum2.mayor1.Length} caracteres");
+        
+        
+        
         }
     }
 }
